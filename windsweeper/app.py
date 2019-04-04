bac4e2eb57d728b36c3598d86288dc5cc04fcf81
Robert Armstrong <robert.armstrong@ga.gov.au>
2019-04-04 14:55:53 +1100

    x-api-key basic check

--- windsweeper/app.py
+++ windsweeper/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request, abort
 from flask_restful import Api
 from windsweeper.resources.products import Product, Products
 from windsweeper.resources.datasets import Dataset, Datasets
 from windsweeper.resources.datacube import Datacube
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +13,18 @@ api.add_resource(Products, "/products", "/products/")
 api.add_resource(Dataset, "/datasets/<string:ds_id>")
 api.add_resource(Datasets, "/datasets", "/datasets/")
 api.add_resource(Datacube, "/datacube")
+
+key = os.getenv("API_KEY", None)
+
+@app.before_request
+def before_request():
+    api_key = request.headers.get('x-api-key', None)
+    if api_key is None:
+        abort(401)
+
+    if api_key != key:
+        abort(401)
+
 
 @app.after_request
 def after_request(response):
