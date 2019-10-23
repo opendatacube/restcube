cb80430bce99be29095f78bad6c5d9b7b1699cfa
pindge <pindge.j@gmail.com>
2019-10-23 22:19:34 +0000

    Temporarily disable indexing

--- restcube/app.py
+++ restcube/app.py
@@ -9,7 +9,7 @@ from restcube.resources.products import Product, Products
 from restcube.resources.datasets import Dataset, Datasets, Locations
 from restcube.resources.datacube import Datacube
 from restcube.resources.tasks import Task, Tasks
-from restcube.resources.index import Index
+#from restcube.resources.index import Index
 from restcube.resources.data import Data
 from restcube.resources.database import Database
 
@@ -26,7 +26,7 @@ api.add_resource(Dataset, "/datasets/<string:ds_id>")
 api.add_resource(Datasets, "/datasets", "/datasets/")
 api.add_resource(Datacube, "/datacube", "/datacube/")
 api.add_resource(Database, "/database", "/database/")
-api.add_resource(Index, "/index", "/index/")
+#api.add_resource(Index, "/index", "/index/")
 api.add_resource(Task, "/tasks/<string:task_id>")
 api.add_resource(Tasks, "/tasks", "/tasks/")
 api.add_resource(Data, "/data", "/data/")
